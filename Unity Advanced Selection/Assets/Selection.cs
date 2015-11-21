1e6cfa0cccf77fa93895a6ade2d7d4eb8a28cb68
Renato Deris Prado <renatoprado21@gmail.com>
2015-11-21 19:19:37 -0200

    Allow multi and single selection

--- Unity Advanced Selection/Assets/Selection.cs
+++ Unity Advanced Selection/Assets/Selection.cs
@@ -1,13 +1,111 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Selection : MonoBehaviour {
 
     public Texture2D selectionHighlight;
-    public static Rect selection = new Rect(0,0,0,0);
+    public static Rect seletionRect = new Rect(0,0,0,0);
     private Vector3 startClick = -Vector3.one;
 
-    private void CheckCamera()
+    public Unit[] units = new Unit[0];
+
+    private Color selectionColor = Color.green;
+
+    private float ScreenToRectSpaceY(float y)
+    {
+        return Screen.height - y;
+    }
+
+    public void Select(Unit unit)
+    {
+        unit.selected = true;
+        unit.GetComponent<Renderer>().material.color = selectionColor;
+    }
+
+    private void Unselect(Unit unit)
+    {
+        unit.selected = false;
+        unit.GetComponent<Renderer>().material.color = unit.materialColor;
+    }
+    
+    private void HandleDragSelection()
+    {
+        foreach(var unit in units)
+        {
+            if(unit.GetComponent<Renderer>().isVisible)
+            {
+                Vector3 camPos = Camera.main.WorldToScreenPoint(unit.transform.position);
+                camPos.y = ScreenToRectSpaceY(camPos.y);
+                
+                if(seletionRect.Contains(camPos))
+                {
+                    Select(unit);
+                }
+                else
+                {
+                    Unselect(unit);
+                }
+
+            }
+            else
+            {
+                Unselect(unit);
+            }
+        }
+    }
+
+    private void HandleClickSelection(Vector3 startClick)
+    {
+        RaycastHit hitInfo = new RaycastHit();
+        bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(startClick), out hitInfo);
+        if (hit) 
+        {
+            Unit unitHit = hitInfo.transform.gameObject.GetComponent<Unit>();
+            if(unitHit != null)
+            {
+                Select(unitHit); 
+                Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+            }
+            foreach(var unit in units)
+            {
+                if(unit != unitHit && unit.selected)
+                {
+                    Unselect(unit);
+                }                  
+            }
+        } 
+        else 
+        {
+            Debug.Log("No hit");
+            foreach(var unit in units)
+            {
+                if(unit.selected)
+                {
+                    Unselect(unit);
+                }
+            }
+        }
+    }
+
+    private void UpdateMultiSelectionRect(Vector3 startClick)
+    {
+        seletionRect = new Rect(startClick.x, ScreenToRectSpaceY(startClick.y),
+                Input.mousePosition.x - startClick.x,
+                ScreenToRectSpaceY(Input.mousePosition.y) - ScreenToRectSpaceY(startClick.y));
+
+        if(seletionRect.width < 0)
+        {
+            seletionRect.x += seletionRect.width;
+            seletionRect.width = -seletionRect.width;
+        }
+        if(seletionRect.height < 0)
+        {
+            seletionRect.y += seletionRect.height;
+            seletionRect.height = -seletionRect.height;
+        }
+    }
+
+    private void HandleSelection()
     {
         if(Input.GetMouseButtonDown(0))
         {
@@ -15,50 +113,32 @@ public class Selection : MonoBehaviour {
         }
         else if(Input.GetMouseButtonUp(0))
         {
-            if(selection.width == 0 && selection.height == 0)
+            if(seletionRect.width == 0 && seletionRect.height == 0)
             {
-                RaycastHit hitInfo = new RaycastHit();
-                bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(startClick), out hitInfo);
-                if (hit) 
-                {
-                    Unit selectedUnit = hitInfo.transform.gameObject.GetComponent<Unit>();
-                    if(selectedUnit != null)
-                    {
-                        //selectedUnit.selected = true;
-                        Debug.Log("Hit " + hitInfo.transform.gameObject.name);
-                    }
-                } 
-                else {
-                    Debug.Log("No hit");
-                }
+                // not multiselecting, ok to perform click selection
+
+                HandleClickSelection(startClick);
             }
 
             startClick = -Vector3.one;
-            selection = new Rect(0,0,0,0);
+            seletionRect = new Rect(0,0,0,0);
         }
 
         if(Input.GetMouseButton(0))
         {
-            selection = new Rect(startClick.x, ScreenToRectSpaceY(startClick.y),
-                    Input.mousePosition.x - startClick.x,
-                    ScreenToRectSpaceY(Input.mousePosition.y) - ScreenToRectSpaceY(startClick.y));
-            
-            if(selection.width < 0)
-            {
-                selection.x += selection.width;
-                selection.width = -selection.width;
-            }
-            if(selection.height < 0)
-            {
-                selection.y += selection.height;
-                selection.height = -selection.height;
+            UpdateMultiSelectionRect(startClick);
+            if(seletionRect.width > 0 || seletionRect.height > 0)
+            { 
+                HandleDragSelection();
             }
         }
     }
 
-    public static float ScreenToRectSpaceY(float y)
+    
+
+    void Update()
     {
-        return Screen.height - y;
+        HandleSelection();
     }
 
     void OnGUI()
@@ -66,25 +146,7 @@ public class Selection : MonoBehaviour {
         if(startClick != -Vector3.one)
         {
             GUI.color = new Color (1,1,1,0.5f);
-            GUI.DrawTexture(selection, selectionHighlight);
+            GUI.DrawTexture(seletionRect, selectionHighlight);
         }
     }
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-    void Update()
-    {
-         
-
-        CheckCamera();
-
-
-
-
-    }
 }
-
-
--- Unity Advanced Selection/Assets/Unit.cs
+++ Unity Advanced Selection/Assets/Unit.cs
@@ -3,30 +3,31 @@ using System.Collections;
 
 public class Unit : MonoBehaviour {
 
-    public bool selected = false;
+    public bool selected{get;set;}
+    public Color materialColor{get;set;}
 
 	// Use this for initialization
 	void Start () {
-	
+        materialColor = GetComponent<Renderer>().material.color;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(GetComponent<Renderer>().isVisible && Input.GetMouseButton(0))
-        {
-            Vector3 camPos = Camera.main.WorldToScreenPoint(transform.position);
-            camPos.y = Selection.ScreenToRectSpaceY(camPos.y);
-            selected = Selection.selection.Contains(camPos);
-        }
+        //if(GetComponent<Renderer>().isVisible && Input.GetMouseButton(0))
+        //{
+        //    Vector3 camPos = Camera.main.WorldToScreenPoint(transform.position);
+        //    camPos.y = Selection.ScreenToRectSpaceY(camPos.y);
+        //    selected = Selection.selection.Contains(camPos);
+        //}
 
-        if(selected)
-        {
-            GetComponent<Renderer>().material.color = Color.red;
-        }
-        else
-        {
-            GetComponent<Renderer>().material.color = Color.white;
-        }
+        //if(selected)
+        //{
+        //    GetComponent<Renderer>().material.color = Color.red;
+        //}
+        //else
+        //{
+        //    GetComponent<Renderer>().material.color = Color.white;
+        //}
 	
 	}
 }
